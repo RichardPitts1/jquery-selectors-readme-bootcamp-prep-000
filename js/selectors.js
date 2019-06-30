f981ff7fc7c0bc01b1cdebae4acc02cca4da7c34
Richard Pitts <richardepitts@gmail.com>
2019-06-30 20:32:06 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,9 +4,17 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  $('img:last')
+  return $('img:last')
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja')
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
+
+function firstListItem() {
+  return $(ul.pic-list first-child )
 }
